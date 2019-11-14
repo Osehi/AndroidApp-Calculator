dbeb5d78a251a018606b0b8c8a8547be3f37e49f
Osehi <osehiaseehilen@gmail.com>
2019-11-14 23:48:07 +0100

    'logic'

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,9 +2,18 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var result:EditText
+    private lateinit var newNumber:EditText
+    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) {findViewById<TextView>(R.id.operation)}
 
+    // variables to hold the operands and type of calculation
+    private var operand1:Double? = null
+    private var operand2: Double = 0.0
+    private var pendingOperation = "="
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
