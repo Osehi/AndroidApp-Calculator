d70de83bdf386df586da1571058938745eb74373
Osehi <osehiaseehilen@gmail.com>
2019-11-16 04:39:13 +0100

    'add-onClickListener-to-button'

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -17,5 +19,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        result = findViewById(R.id.result)
+        newNumber = findViewById(R.id.newnumber)
+
+        // Data input buttons
+        val button0:Button = findViewById(R.id.button0)
+        val button1:Button = findViewById(R.id.button1)
+        val button2:Button = findViewById(R.id.button2)
+        val button3:Button = findViewById(R.id.button3)
+        val button4:Button = findViewById(R.id.button4)
+        val button5:Button = findViewById(R.id.button5)
+        val button6:Button = findViewById(R.id.button6)
+        val button7:Button = findViewById(R.id.button7)
+        val button8:Button = findViewById(R.id.button8)
+        val button9:Button = findViewById(R.id.button9)
+        val buttonDot:Button = findViewById(R.id.buttonDot)
+
+        // Operation buttons
+        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
+        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+
+        val listener = View.OnClickListener{v ->
+            val b = v as Button
+            newNumber.append(b.text)
+        }
+
+        // assign this listener to every button so that it is called when one of them is clicked
+
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
+        buttonDot.setOnClickListener(listener)
     }
 }
